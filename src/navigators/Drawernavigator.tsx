4a525e3ca07bcaf61ce78a72142149591c3ef33f
abdullah094 <abdullahfarooqui094@gmail.com>
2023-06-02 18:10:14 +0500

    new proj

---
+++ src/navigators/Drawernavigator.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Drawernavigator = () => {
+  return (
+    <View>
+      <Text>Drawernavigator</Text>
+    </View>
+  );
+};
+
+export default Drawernavigator;
+
+const styles = StyleSheet.create({});
