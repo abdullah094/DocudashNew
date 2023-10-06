ab9f1b639ce14befa82aa1a269a2f3d65c5aefc9
darkwaqar <urspecial1one@gmail.com>
2023-10-06 22:00:45 +0500

    User Request List and Details

---
+++ src/components/UserRequestList.tsx
@@ -0,0 +1,104 @@
+import FilterModal from '@components/FilterModal';
+import { selectAccessToken } from '@stores/Slices';
+import { Envelope, InboxApiResponse } from '@type/index';
+import axios from 'axios';
+import SkeletonLoader from 'expo-skeleton-loader';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { Divider } from 'react-native-paper';
+import { useSelector } from 'react-redux';
+import tw from 'twrnc';
+import EnvelopeListItem from './EnvelopeListItem';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import UserRequestListItem from './UserRequestListItem';
+export default function UserRequestList() {
+  const navigation = useNavigation();
+  const focused = useIsFocused();
+  const accessToken = useSelector(selectAccessToken);
+  const [data, setData] = useState<Array<UserRequestListT>>();
+  const [loading, setLoading] = useState(false);
+  console.log(accessToken);
+
+  function filter(name: string | undefined) {
+    if (name) {
+      const filtered = data.filter((x) => x.requestMessage.includes(name));
+      return filtered;
+    }
+  }
+  const [Name, setName] = useState<string | undefined>();
+  const fetchData = async () => {
+    setLoading(true);
+    const url = 'https://docudash.net/api/individuals/RequestsList';
+    await axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        setLoading(false);
+        const data = response.data;
+        console.log('request data', data);
+        setData(data.data);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log('Error----', error);
+        setData([]);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [focused]);
+  const onRefresh = () => {
+    fetchData();
+  };
+  return (
+    <FlatList
+      data={loading ? new Array(7).fill({}) : filter(Name) ? filter(Name) : data}
+      ListHeaderComponent={
+        <View style={tw`flex-row items-center justify-between px-5 mb-5 `}>
+          <Text style={tw`font-bold text-6  tracking-wider`} numberOfLines={1}>
+            Requests
+          </Text>
+          <FilterModal
+            onPress={(item: React.SetStateAction<string | undefined>) => setName(item)}
+          />
+        </View>
+      }
+      onRefresh={onRefresh}
+      refreshing={loading}
+      ItemSeparatorComponent={Divider}
+      contentContainerStyle={[tw`pb-25 py-5`, { alignSelf: 'stretch' }]}
+      ListEmptyComponent={
+        <View>
+          <Text style={tw`text-center text-gray-500`}>No Request items Found</Text>
+        </View>
+      }
+      //   keyExtractor={(item) => item.id + '_'}
+      renderItem={({ item }) => (loading ? <Skeleton /> : <UserRequestListItem item={item} />)}
+    />
+  );
+}
+
+const Skeleton = () => {
+  return (
+    <SkeletonLoader boneColor={'#D3D3D3'}>
+      <SkeletonLoader.Container style={tw`my-2`}>
+        <SkeletonLoader.Container style={tw`flex-row overflow-hidden gap-2 items-center px-5 `}>
+          {/*  @ts-ignore */}
+          <SkeletonLoader.Item style={tw`w-10 h-10 rounded-full`} />
+          <SkeletonLoader.Container style={tw`overflow-hidden w-70`}>
+            {/*  @ts-ignore */}
+            <SkeletonLoader.Item style={tw`flex-1 h-5 my-1`} />
+            {/*  @ts-ignore */}
+            <SkeletonLoader.Item style={tw`w-100 h-2`} />
+          </SkeletonLoader.Container>
+        </SkeletonLoader.Container>
+        {/*  @ts-ignore */}
+        <SkeletonLoader.Item style={tw`w-80 h-5 self-center mt-5`} />
+      </SkeletonLoader.Container>
+    </SkeletonLoader>
+  );
+};
---
+++ src/components/UserRequestListItem.tsx
@@ -0,0 +1,47 @@
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { IRequestItem, RootStackScreenProps } from '@type/index';
+import React from 'react';
+import { Image, TouchableOpacity, View } from 'react-native';
+import tw from 'twrnc';
+import { Text } from 'react-native-paper';
+import { reasons } from '@utils/requestReason';
+import { time } from '@utils/requestTime';
+interface IEnvelopeListItem {
+  item: IRequestItem;
+}
+export default function UserRequestListItem({ item }: IEnvelopeListItem) {
+  console.log(item);
+  const navigation = useNavigation<RootStackScreenProps<'Inbox'>['navigation']>();
+  const route = useRoute<RootStackScreenProps<'Inbox'>['route']>();
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate('RequestDetails', { id: item.id })}>
+      <View style={tw` mx-2 gap-2 rounded-lg p-4 justify-between bg-white my-2 shadow-lg`}>
+        <Text variant="bodyMedium" numberOfLines={2}>
+          Request: {''}
+          {reasons.find((x) => x.value == item.reasonOfRequest.toString()).label}
+        </Text>
+        <Text variant="labelLarge" numberOfLines={2}>
+          Message: {''}
+          <Text style={tw`font-thin`}>{item.requestMessage}</Text>
+        </Text>
+        <Text variant="labelLarge">
+          Request Location: {''}
+          <Text style={tw`font-thin`}>{item.request_location_list.name}</Text>
+        </Text>
+        <Text variant="labelLarge">
+          Request Time: {''}
+          <Text style={tw`font-thin`}>
+            {' '}
+            {time.find((x) => x.value == item.requestTime.toString()).label}
+          </Text>
+        </Text>
+        <Text variant="labelLarge">
+          From User:{' '}
+          <Text style={tw`font-thin`}>
+            {item.notary_details.name + ' ' + item.notary_details.last_name}
+          </Text>
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
